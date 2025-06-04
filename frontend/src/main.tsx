2a2562a55069d1568313358fc9705d66e6f22454
Shabrina Mim <mimba007@gmail.com>
2025-06-04 17:09:19 +0600

    refactored frontend project and added Dockerfile

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
+import { ApolloProvider } from '@apollo/client'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css'
 import App from './App.tsx'
+import { client } from './graphql/client.ts'
+import './styles/styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
+  </StrictMode>
 )
